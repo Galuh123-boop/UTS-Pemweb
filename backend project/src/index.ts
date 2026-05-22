e285c7705e5df77134dc34416798502010180bcf
Galuh123-boop <galuhgunawan914@gmail.com>
2026-05-22 21:50:57 +0700

    update terus

--- backend project/src/index.ts
+++ backend project/src/index.ts
@@ -1,25 +1,21 @@
 import express from 'express';
 import cors from 'cors';
 
-import eventRoutes from './routes/eventRoute.js';
-import categoryRoutes from './routes/categoryRoute.js';
-import pembicaraRoutes from './routes/pembicaraRoute.js';
+import eventRoutes from '../src/routes/eventRoute.js';
+import categoryRoutes from '../src/routes/categoryRoute.js';
+import pembicaraRoutes from '../src/routes/pembicaraRoute.js';
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Route utama
 app.get('/', (req, res) => {
-  res.status(200).send('Ini adalah API untuk Invofest');
+  res.send('Ini adalah API untuk Invofest');
 });
 
-// Routes API
 app.use('/events', eventRoutes);
 app.use('/category', categoryRoutes);
 app.use('/pembicara', pembicaraRoutes);
 
-// Export app untuk Vercel Serverless
 export default app;
